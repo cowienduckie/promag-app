b0358ef13c4184cd18fc29cffe4f383765e1ae7c
Minh Tran <minhtran.2404@outlook.com>
2022-12-28 12:46:46 +0700

    fix: linting settings

--- src/utils/lazyImport.ts
+++ src/utils/lazyImport.ts
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 export function lazyImport<
-  T extends React.ComponentType<any>,
+  T extends React.ComponentType<unknown>,
   I extends { [K2 in K]: T },
   K extends keyof I
 >(factory: () => Promise<I>, name: K): I {
