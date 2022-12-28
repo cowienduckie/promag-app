cafa4e7102d91e0580e6b72e3103efa3eacaaa0d
Minh Tran <minhtran.2404@outlook.com>
2022-12-28 15:17:36 +0700

    feat: landing page

--- src/features/misc/routes/Landing.tsx
+++ src/features/misc/routes/Landing.tsx
@@ -1,3 +1,18 @@
+import { SendOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+
 export const Landing = () => {
-  return <h1>HOME PAGE</h1>;
+  return (
+    <div className="m-auto flex min-h-screen flex-col items-center justify-center">
+      <h1 className="mb-10 text-center text-4xl font-bold">
+        Welcome to the landing page!
+      </h1>
+      <Link to="/app">
+        <Button type="primary" size="large">
+          Go to App <SendOutlined className="align-baseline" />
+        </Button>
+      </Link>
+    </div>
+  );
 };
